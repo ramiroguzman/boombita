80757d4f03c4f901908e0b6b39462f0303d6609b
U-W7-PC\ramiro <ramiro@W7-PC.(none)>
2014-02-24 00:50:42 -0600

    first commit

--- src/boombita/app.js
+++ src/boombita/app.js
@@ -9,6 +9,10 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/boombita');
+
 var app = express();
 
 // all environments
@@ -30,9 +34,15 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// routes registration
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/helloworld', routes.helloworld);
+app.get('/userlist', routes.userlist(db));
+app.get('/newuser', routes.newuser);
+app.post('/adduser', routes.adduser(db));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
